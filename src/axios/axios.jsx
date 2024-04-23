43c465d6db01b592f89e3352b7df7179b8db3fc2
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-04-24 03:39:40 +0600

    solve-some-little-issue

--- src/axios/axios.jsx
+++ src/axios/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://convo-app-server-side.vercel.app/api/v1",
 });
 
 export default instance;
