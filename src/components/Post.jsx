d7bb25d6b0a08d212f80bffab20c8cc93902e38d
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-04-23 19:57:33 +0600

    implemented-post-edit-and-update-authenticated-user

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -6,15 +6,28 @@ import toast from "react-hot-toast";
 import useAuth from "@/hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 import TimeAgo from "./TimeAgo";
+import PostAction from "./PostAction";
+import { useRef, useState } from "react";
 export default function Post({ post }) {
+  const [editable, setEditable] = useState(false);
+  const updateInputRef = useRef();
   const { authUser } = useAuth();
   const { _id, content, user, like, createdAt } = post || {};
-  const { name, avatar } = user || {};
+  const { name, avatar, email } = user || {};
   const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { mutateAsync: addLike } = useMutation({
     mutationFn: async () => {
       return await axios.patch(`/posts/add-like/${_id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["Posts"]);
+    },
+  });
+
+  const { mutateAsync: updateContent } = useMutation({
+    mutationFn: async (data) => {
+      return await axios.patch(`/posts/${_id}`, data);
     },
     onSuccess: () => {
       queryClient.invalidateQueries(["Posts"]);
@@ -32,24 +45,53 @@ export default function Post({ post }) {
       toast.error(error.message);
     }
   };
+
+  const handleUpdatePost = async (e) => {
+    e.preventDefault();
+    try {
+      await updateContent({ content: updateInputRef.current.value });
+      toast.success("Post is updated successfully!!");
+      setEditable(false);
+    } catch (error) {
+      toast.error(error.message);
+      setEditable(false);
+    }
+  };
+
   return (
     <div className="border-b">
       <div className="bg-white p-6">
-        <div className="flex space-x-4">
-          <img
-            className="w-12 h-12 object-cover rounded-full"
-            src={avatar}
-            alt=""
-          />
-          <div>
-            <h3 className="text-xl font-semibold">{name}</h3>
-            <p className=" text-sm text-gray-600">
-              <TimeAgo date={createdAt} />
-            </p>
+        <div className="flex justify-between items-center">
+          <div className="flex space-x-4">
+            <img
+              className="w-12 h-12 object-cover rounded-full"
+              src={avatar}
+              alt=""
+            />
+            <div>
+              <h3 className="text-xl font-semibold">{name}</h3>
+              <p className=" text-sm text-gray-600">
+                <TimeAgo date={createdAt} />
+              </p>
+            </div>
           </div>
+          {authUser?.email === email && (
+            <PostAction setEditable={setEditable} id={_id} />
+          )}
         </div>
         <div className="py-5">
-          <p className="text-base font-medium">{content}</p>
+          {editable ? (
+            <form onSubmit={handleUpdatePost}>
+              <input
+                className="w-full px-5 py-2 rounded-md border border-black"
+                ref={updateInputRef}
+                defaultValue={content}
+              />
+              <input hidden type="submit" value="submit" />
+            </form>
+          ) : (
+            <p className="text-base font-medium">{content}</p>
+          )}
         </div>
         <div>
           <Button onClick={() => handleAddLikePost()} variant="outline">
---
+++ src/components/PostAction.jsx
@@ -0,0 +1,82 @@
+/* eslint-disable react/prop-types */
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import axios from "@/axios/axios";
+import Swal from "sweetalert2";
+
+export default function PostAction({ setEditable, id }) {
+  const queryClient = useQueryClient();
+  const { mutateAsync: deletePost } = useMutation({
+    mutationFn: async () => {
+      return await axios.delete(`/posts/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["Posts"]);
+    },
+  });
+
+  const handleDeletePost = async () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          await deletePost();
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        } catch (error) {
+          Swal.fire({
+            icon: "error",
+            title: error,
+            text: "Post is deleted failed",
+          });
+        }
+      }
+    });
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
+          </svg>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-10">
+        <DropdownMenuItem
+          onClick={() => setEditable(true)}
+          className="cursor-pointer font-medium"
+        >
+          Edit Post
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => handleDeletePost()}
+          className="cursor-pointer font-medium"
+        >
+          Delete Post
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
